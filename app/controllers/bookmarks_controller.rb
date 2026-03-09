2c1d8d5a64487b8742a52bc5d21ac16135170da6
Joana Dias <joana.jou@gmail.com>
2026-03-09 16:42:04 -0300

    railswatchlist2

---
+++ app/controllers/bookmarks_controller.rb
@@ -0,0 +1,30 @@
+class BookmarksController < ApplicationController
+  def new
+    @list = List.find(params[:list_id])
+    @bookmark = Bookmark.new
+  end
+
+  def create
+    @list = List.find(params[:list_id])
+    @bookmark = Bookmark.new(bookmark_params)
+    @bookmark.list = @list
+
+    if @bookmark.save
+      redirect_to list_path(@list)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end  # ← Added this missing end for the create method
+
+  def destroy
+    @bookmark = Bookmark.find(params[:id])
+    @bookmark.destroy
+    redirect_to list_path(@bookmark.list)
+  end
+
+  private
+
+  def bookmark_params
+    params.require(:bookmark).permit(:comment, :movie_id)
+  end
+end  # ← This ends the class
